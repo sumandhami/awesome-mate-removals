048931093e78d13b683dbf10a5aca5591e1aeac0
alpineroottech <alpineroottech@gmail.com>
2026-04-24 07:15:57 +0545

    perf(homepage): lazy load non-critical sections

--- src/PageViews/Home1/Home1.jsx
+++ src/PageViews/Home1/Home1.jsx
@@ -1,19 +1,21 @@
 "use client";
 
 import { useEffect } from "react";
+import dynamic from "next/dynamic";
 import About from "../../Componant1/About/About";
 import Banner from "../../Componant1/Banner/Banner";
-import Blog from "../../Componant1/Blog/Blog";
-import HomeFaq from "../../Componant1/Faq/HomeFaq";
 import Feature from "../../Componant1/Feature/Feature";
-import MovingCost from "../../Componant1/MovingCost/MovingCost";
-import Portfolio from "../../Componant1/Portfolio/Portfolio";
-import ProcessCard from "../../Componant1/Process/Process";
-import ServiceAreas from "../../Componant1/ServiceAreas/ServiceAreas";
 import Service from "../../Componant1/Service/Service";
-import Support from "../../Componant1/Support/Support";
-import Testimonial from "../../Componant1/Testimonial/Testimonial";
-import WhyChoose from "../../Componant1/WhyChoose/WhyChoose";
+
+const ProcessCard = dynamic(() => import("../../Componant1/Process/Process"));
+const MovingCost = dynamic(() => import("../../Componant1/MovingCost/MovingCost"));
+const WhyChoose = dynamic(() => import("../../Componant1/WhyChoose/WhyChoose"));
+const ServiceAreas = dynamic(() => import("../../Componant1/ServiceAreas/ServiceAreas"));
+const Blog = dynamic(() => import("../../Componant1/Blog/Blog"));
+const Testimonial = dynamic(() => import("../../Componant1/Testimonial/Testimonial"));
+const Portfolio = dynamic(() => import("../../Componant1/Portfolio/Portfolio"));
+const HomeFaq = dynamic(() => import("../../Componant1/Faq/HomeFaq"));
+const Support = dynamic(() => import("../../Componant1/Support/Support"));
 
 const Home1 = () => {
   useEffect(() => {
