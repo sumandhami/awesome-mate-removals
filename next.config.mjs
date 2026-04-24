6a9866a22e5f80a1cd146692c26693927f0d0d25
anjanuprety <anjanuprety787@gmail.com>
2026-04-24 17:02:45 +0545

    updated turnstiles csp

--- next.config.mjs
+++ next.config.mjs
@@ -14,9 +14,11 @@ const cspDirectives = [
   "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
   "img-src 'self' data: blob: https:",
   "font-src 'self' data: https://fonts.gstatic.com",
+  // ADDED https://challenges.cloudflare.com HERE:
   `connect-src 'self'${
     isProduction ? "" : " ws: wss:"
-  } https://www.google.com https://www.gstatic.com https://www.recaptcha.net`,
+  } https://www.google.com https://www.gstatic.com https://www.recaptcha.net https://challenges.cloudflare.com`,
+  // ADDED https://challenges.cloudflare.com HERE:
   "frame-src https://www.google.com https://www.recaptcha.net https://challenges.cloudflare.com",
   "media-src 'self' data: https://commondatastorage.googleapis.com",
   "base-uri 'self'",
